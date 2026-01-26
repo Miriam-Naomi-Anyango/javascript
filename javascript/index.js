76d42a2b307166af86646761cb4e29a66c17485e
miriam <minaoanya@gmail.com>
2026-01-26 10:33:35 +0300

    Point noted

--- javascript/index.js
+++ javascript/index.js
@@ -18,3 +18,25 @@ let secondName = 'Naomi', thirdName = 'Anyango';
 console.log(thirdName);
 //In order therefore to have a variable if it has many names
 //  use a camel case while combining the names
+
+//If you never want the value of a variable to change, declare
+//it using the const keyword (referring to aconstant) and not the let
+//keyword, otherwise it will change
+
+//for example if you write
+
+let dogName = 'Bosco';
+dogName = 'Skipy';
+
+console.log(dogName);
+
+//This will give you Skipy and not Bosco.
+//Therefore if you don't want it to change, you can consider doing
+
+const catName = 'Chilly';
+//catName = 'Kibely';
+
+console.log(catName);
+//This will throw an error
+
+//The value of a variable can change whereas the value of a constant can never change
