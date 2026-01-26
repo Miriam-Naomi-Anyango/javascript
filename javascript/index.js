99c5fcdb55d07e0704f954aaf1a64456ca00eddd
miriam <minaoanya@gmail.com>
2026-01-26 10:19:25 +0300

    Rules of writing variables

--- javascript/index.js
+++ javascript/index.js
@@ -4,3 +4,12 @@ let firstName = 'Miriam';
 console.log(firstName);
 firstName = 'Naomi';
 console.log(firstName);
+
+//When declaring variables consider the following rules
+//A variable cannot be a reserved keyword
+//It should be meaningful
+//Cannot begin with a number
+//A variable cannot contain a space or a hyphen.
+
+//In order therefore to have a variable if it has many names
+//  use a camel case while combining the names
