c64db9c032e2da93df0ad0133072994d0860af28
miriam <minaoanya@gmail.com>
2026-01-26 10:24:58 +0300

    Got the concept of declaring more than one variable twice

--- javascript/index.js
+++ javascript/index.js
@@ -10,6 +10,11 @@ console.log(firstName);
 //It should be meaningful
 //Cannot begin with a number
 //A variable cannot contain a space or a hyphen.
+//Variables are case sensitive
+//if you want to initialize variable with more than two names you can use comas
 
+//For example
+let secondName = 'Naomi', thirdName = 'Anyango';
+console.log(thirdName);
 //In order therefore to have a variable if it has many names
 //  use a camel case while combining the names
